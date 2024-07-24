0b584406f38a0bf5a3696ff8ac85da391d48a07b
suraj1234arya <suraj.arya@mobulos.com>
2024-07-24 12:49:04 +0530

    commit

--- crypto.js
+++
@@ -1,9 +0,0 @@
-const crypto = require('crypto');
-
-// Create an HMAC
-const hmac = crypto.createHmac('sha256', 'a secret key');
-// Update the HMAC with data
-hmac.update('some data to hash');
-// Calculate the digest of the data
-const digest = hmac.digest('hex');
-console.log(`HMAC: ${digest}`);
